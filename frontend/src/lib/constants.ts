22201150d36f163ed5beb7a1426ecdcdaeef91f2
Hugo Peixoto <hugopeixoto@loba.com>
2026-05-15 15:04:31 +0000

    frontend/src/lib/constants.ts edited online with Bitbucket

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const API_BASE_URL = window.location.hostname === 'localhost'
   ? 'http://localhost:4043/api'
-  : `https://api.${window.location.hostname}/api`;
+  : `https://api.${window.location.hostname}`;
